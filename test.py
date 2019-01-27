734e140ff24675c6bfb54104522fb88316570439
cjh <1418503647@qq.com>
2019-01-27 19:18:48 +0800

    mac

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-print("Hello, World!");
+# print("Hello, World!");
 
-for letter in 'asd':
-    print(letter)
+# for letter in 'asd':
+#     print(letter)
+
+import jieba
+s = u'我想和女朋友一起去北京故宫博物院参观和闲逛。'
+
+cut = jieba.cut(s)
+
+print('【Output】')
+print(cut)
+print(','.join(cut))
